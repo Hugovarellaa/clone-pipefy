86473b5d61b71ed158e7fd4b680be3326d812482
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-29 22:52:56 -0300

    .
    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import { AppProps } from "next/app";
-import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>ig.news</title>
-      </Head>
+     
 
       <Component {...pageProps} />
     </>
