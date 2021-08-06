8aeea52cc7dda526c797d75cfd08735444d791c7
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-08-06 16:59:07 -0300

    .
    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -36,11 +36,12 @@ export default function Home({ product }:HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1J1gsKAZhJCoN0MQyHVkGvL3");
 
   const product = {
     priceId: price.id,
+    //formatando o preço 
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD'
@@ -51,5 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60* 60* 24 // 24-hours
   };
 };
