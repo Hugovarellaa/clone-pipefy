f1fa9cc2c7f1021cb019d41fb74d8706b6295037
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-08-06 11:29:49 -0300

    Delete src directory

--- src/services/stripe-js.ts
+++
@@ -1,7 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js'
-
-export async function getStripeJs(){
-    const stripejs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
-
-    return stripejs;
-}
