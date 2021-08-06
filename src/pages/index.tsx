728a891b0ca00cff290506010408ebf284a7bf2d
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-08-06 12:30:30 -0300

    .
    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-
+import styles from '../styles/global.module.scss';
+import Head from 'next/head'
 
 export default function Home() {
   return (
-     <h1> Ola mundo!!</h1>
+     <h1 className={styles.title}> 
+     <Head>
+       <title>Home | Ignews</title>
+     </Head>
+     Ola mundo!!</h1>
   )
 }
