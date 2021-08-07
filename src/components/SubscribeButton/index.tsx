10d915ae38bb1d566a2ee53bce492b8b6f44bdcf
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-08-07 16:27:39 -0300

    .
    .

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,27 @@
-import styles from './styles.module.scss';
+import { useSession, signIn } from "next-auth/client";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string;
+  priceId: string;
 }
 
-export function SubscribeButton( {priceId}: SubscribeButtonProps){
-    return (
-        <button
-            type="button"
-            className={styles.subscribeButton}
-        >
-            Subscribe now
-        </button>
-    )
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  function handleSubscribe() {
+      if(!session){
+        signIn('github')
+        return
+      }
+      //criacao da checkout session
+  }
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
