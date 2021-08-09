bb8f3692045502d57642ce8da79801441b67b5a8
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-08-09 16:34:13 -0300

    Pagamentos no Stripe
    3 aulas 33:24 completas

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,19 +1,56 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { stripe } from '../../services/stripe'
 import { getSession } from 'next-auth/client'
+import { faunadb } from '../../services/fauna'
+import { query as q } from 'faunadb'
+
+
+interface User {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_customer_id: string;
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const session = await getSession({ req })
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            //metadata:
-        })
+        const user = await faunadb.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
 
+        let customerId = user.data.stripe_customer_id
+        if(!customerId){
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                //metadata:
+            })
 
+            await faunadb.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+            customerId = stripeCustomer.id
+        }
+
+        
+       
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
