3f4d534983681c36d272e2b97cb46b5a36c9c5d2
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-08-16 20:43:28 -0300

    .
    .

---
+++ src/components/Model/ModelSection/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+const ModelSection: React.FC = () => {
+  return (
+    <Container>
+      <h1>asdfa</h1>
+    </Container>
+  );
+};
+
+export default ModelSection;
--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import DefaultOverlayContent from '../DefaultOverlayContent';
 import { Container } from './styles';
 
 const Page: React.FC = () => {
   return (
     <Container>
-      <h1>Page</h1>
+     <ModelsWrapper>
+       <div>
+         <ModelSection 
+          modelName="Model One"
+          overlayNode={
+            <DefaultOverlayContent 
+              label="Model One"
+              description="Order Online for Delivery"
+            />
+          }
+         />
+       </div>
+     </ModelsWrapper>
     </Container>
   )
 }
