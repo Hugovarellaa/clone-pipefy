1bd0a70170b6897798190d4e49d5c2885b8c6b96
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-04 19:03:46 -0300

    .
    .

--- src/components/Model/ModelWrapper/index.tsx
+++ src/components/Model/ModelWrapper/index.tsx
@@ -1,13 +1,15 @@
-
+import { useCallback, useRef, useState } from "react";
+import { CarModel } from "../../ModelsContext";
 import { Container } from "./styles";
 
+const ModelWrapper: React.FC = ({ children }) => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([]);
 
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels(state => [...state, model ]);
+  }, []);
 
-const ModelWrapper: React.FC = ({children}) => {
-  return (
-    <Container>
-      {children}
-    </Container>
-  );
+  return <Container ref={wrapperRef}>{children}</Container>;
 };
 export default ModelWrapper;
