3902c10e40b52994f2af460235375fe35b51b6b5
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-04 19:25:36 -0300

    .
    .

--- src/components/Model/ModelWrapper/index.tsx
+++ src/components/Model/ModelWrapper/index.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useRef, useState } from "react";
-import { CarModel } from "../../ModelsContext";
+import ModelsContext, { CarModel } from "../../ModelsContext";
 import { Container } from "./styles";
 
 const ModelWrapper: React.FC = ({ children }) => {
@@ -7,9 +7,32 @@ const ModelWrapper: React.FC = ({ children }) => {
   const [registeredModels, setRegisteredModels] = useState<CarModel[]>([]);
 
   const registerModel = useCallback((model: CarModel) => {
-    setRegisteredModels(state => [...state, model ]);
+    setRegisteredModels((state) => [...state, model]);
   }, []);
 
-  return <Container ref={wrapperRef}>{children}</Container>;
+  const unregisterModel = useCallback((modelName: string) => {
+    setRegisteredModels((state) =>
+      state.filter((model) => model.modelName !== modelName)
+    );
+  }, []);
+
+  const getModelByName = useCallback((modelName: string)=>{
+    return registeredModels.find(item => item.modelName === modelName) || null
+
+  },[registeredModels])
+
+  return (
+    <ModelsContext.Provider
+      value={{
+        wrapperRef,
+        registeredModels,
+        registerModel,
+        unregisterModel,
+        getModelByName,
+      }}
+    >
+      <Container ref={wrapperRef}>{children}</Container>;
+    </ModelsContext.Provider>
+  );
 };
 export default ModelWrapper;
